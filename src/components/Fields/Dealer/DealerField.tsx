c04bcec6906d30e20a0021cccfd92e6148cd453f
Daniel Deak <ddeak@insights.com>
2020-08-14 11:47:48 +0100

    Added Card display for the dealer. Currently the first card will always be 'Hidden'.

--- src/components/Fields/Dealer/DealerField.tsx
+++ src/components/Fields/Dealer/DealerField.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
+import styles from '../Fields.module.scss';
 import Field from '../Field';
+import { Card } from '../../../types/card';
+import { Card as CardView, CustomCard } from '../../Card';
+import { RootState } from '../../../state/store';
 
 const DealerField = () => {
-  return <Field>Dealer:</Field>;
+  const { dealerCards } = useSelector((state: RootState) => state.game);
+
+  return <DealerFieldView cards={dealerCards} />;
+};
+
+type PropsType = {
+  cards: Card[];
+};
+
+const DealerFieldView = ({ cards }: PropsType) => {
+  return (
+    <Field>
+      <p>Dealer Cards:</p>
+      <div className={styles.cardRow}>
+        {cards.map((card, index) =>
+          index === 0 ? (
+            <CustomCard message="Hidden" />
+          ) : (
+            <CardView card={card} />
+          )
+        )}
+      </div>
+    </Field>
+  );
 };
 
 export default DealerField;
