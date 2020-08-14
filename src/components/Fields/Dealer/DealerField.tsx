7102df7169163d612e8df247094ac901de8e0d46
Daniel Deak <ddeak@insights.com>
2020-08-14 12:13:30 +0100

    Refactored to remove View components from player and dealer fields. Currently the state is very minimal.

--- src/components/Fields/Dealer/DealerField.tsx
+++ src/components/Fields/Dealer/DealerField.tsx
@@ -4,26 +4,17 @@ import { useSelector } from 'react-redux';
 
 import styles from '../Fields.module.scss';
 import Field from '../Field';
-import { Card } from '../../../types/card';
 import { Card as CardView, CustomCard } from '../../Card';
 import { RootState } from '../../../state/store';
 
 const DealerField = () => {
   const { dealerCards } = useSelector((state: RootState) => state.game);
 
-  return <DealerFieldView cards={dealerCards} />;
-};
-
-type PropsType = {
-  cards: Card[];
-};
-
-const DealerFieldView = ({ cards }: PropsType) => {
   return (
     <Field>
       <p>Dealer Cards:</p>
       <div className={styles.cardRow}>
-        {cards.map((card, index) =>
+        {dealerCards.map((card, index) =>
           index === 0 ? (
             <CustomCard key="dealerCard" message="Hidden" />
           ) : (
--- src/components/Fields/Player/PlayerField.tsx
+++ src/components/Fields/Player/PlayerField.tsx
@@ -3,26 +3,17 @@ import { useSelector } from 'react-redux';
 
 import styles from '../Fields.module.scss';
 import Field from '../Field';
-import { Card } from '../../../types/card';
 import { Card as CardView } from '../../Card';
 import { RootState } from '../../../state/store';
 
 const PlayerField = () => {
   const { playerCards } = useSelector((state: RootState) => state.game);
 
-  return <PlayerFieldView cards={playerCards} />;
-};
-
-type PropsType = {
-  cards: Card[];
-};
-
-const PlayerFieldView = ({ cards }: PropsType) => {
   return (
     <Field>
       <p>Player Cards:</p>
       <div className={styles.cardRow}>
-        {cards.map((card) => (
+        {playerCards.map((card) => (
           <CardView key={`${card.name} ${card.suit}`} card={card} />
         ))}
       </div>
